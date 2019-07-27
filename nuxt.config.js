7e207028cb81f19af6622a6b5864d50f00f405c5
Olatunji <adeiyiakala91@gmail.com>
2019-07-27 18:11:00 +0100

    module added

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,8 +23,10 @@ module.exports = {
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
-  modules: [],
-  plugins: [],
+  modules: [
+    'nuxt-babel',
+  ],
+    plugins: [],
   /*
   /*
   ** Build configuration
